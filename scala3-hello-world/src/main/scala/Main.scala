72f0372a41deb8a1a83f8f0944354bfe6a17bd51
m.yuki <matsutani.r.communication@gmail.com>
2022-04-26 23:09:13 +0900

    [change] Scala 2 -> Scala 3

--- scala3-hello-world/src/main/scala/Main.scala
+++ scala3-hello-world/src/main/scala/Main.scala
@@ -1,8 +1,5 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
-    println(msg)
-  }
+@main def hello: Unit = 
+  println("Hello world!")
+  println(msg)
 
-  def msg = "I was compiled by Scala 3. :)"
-}
+def msg = "I was compiled by Scala 3. :)"
