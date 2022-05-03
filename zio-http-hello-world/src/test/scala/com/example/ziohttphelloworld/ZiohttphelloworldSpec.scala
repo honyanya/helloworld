d3dac1906d70b498fd9ca591d24c5c93ca4e4599
m.yuki <matsutani.r.communication@gmail.com>
2022-05-03 23:00:10 +0900

    [add] zio-http-hello-world

---
+++ zio-http-hello-world/src/test/scala/com/example/ziohttphelloworld/ZiohttphelloworldSpec.scala
@@ -0,0 +1,16 @@
+package com.example.ziohttphelloworld
+
+import zio.test._
+import zio.test.Assertion._
+import zhttp.http._
+
+object ZiohttphelloworldSpec extends DefaultRunnableSpec {
+  override def spec: ZSpec[Environment, Failure] = suite("""ZiohttphelloworldSpec""")(
+    testM("200 ok") {
+      checkAllM(Gen.fromIterable(List("text", "json"))) { uri =>
+        val request = Request(Method.GET, URL(!! / uri))
+        assertM(ZhttpService.app(request).map(_.status))(equalTo(Status.OK))
+      }
+    },
+  )
+}
