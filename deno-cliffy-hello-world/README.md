da3b83e71b06c3fb64a433c561709c5355399abb
m.yuki <matsutani.r.communication@gmail.com>
2022-06-24 20:14:41 +0900

    [add] deno-cliffy-hello-world

---
+++ deno-cliffy-hello-world/README.md
@@ -0,0 +1,8 @@
+# deno-cliffy-hello-world
+
+```sh
+$ deno run command.ts helloworld
+Check file:///home/vagrant/workspace/src/github.com/honyanya/helloworld/deno-cliffy-hello-world/command.ts
+{ args: [ "helloworld" ] }
+{ options: {} }
+```
---
+++ deno-cliffy-hello-world/command.ts
@@ -0,0 +1,11 @@
+import { Command } from "https://deno.land/x/cliffy@v0.24.2/command/mod.ts";
+
+const { options, args } = await new Command()
+  .name("deno-cliffy-hello-world")
+  .version("0.1.0")
+  .description("cliffy hello world!")
+  .arguments("<arg>")
+  .parse(Deno.args);
+
+console.log({ args });
+console.log({ options });
