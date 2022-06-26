6c13e4e590a801a57e3eeb00b157f8db1d235382
m.yuki <matsutani.r.communication@gmail.com>
2022-06-26 23:14:37 +0900

    [add] deno-cliffy-hello-world add option

--- deno-cliffy-hello-world/command.ts
+++ deno-cliffy-hello-world/command.ts
@@ -5,6 +5,7 @@ const { options, args } = await new Command()
   .version("0.1.0")
   .description("cliffy hello world!")
   .arguments("<arg>")
+  .option("--name <name:string>", "name", { required: true })
   .parse(Deno.args);
 
 console.log({ args });
