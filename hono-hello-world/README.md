53f4bcfa4b7378fca99b7091a6485ac49598333a
honyanya <matsutani.r.communication@gmail.com>
2022-08-05 17:51:02 +0900

    [add] query param.

--- hono-hello-world/README.md
+++ hono-hello-world/README.md
@@ -11,6 +11,9 @@ Hello! Hono!
 $ curl -XGET http://localhost:8787/entry/honyanya
 id is honyanya.
 
+$ curl -XGET "http://localhost:8787/search?q=hono"
+{"query":"hono"}
+
 $ curl -XPOST http://localhost:8787/
 post request.
 ```
--- hono-hello-world/src/index.ts
+++ hono-hello-world/src/index.ts
@@ -5,7 +5,11 @@ const app = new Hono();
 app.get("/", (c) => c.text("Hello! Hono!"));
 app.get("/entry/:id", (c) => {
     return c.text(`id is ${c.req.param("id")}.`);
-})
+});
+app.get("/search", (c) => {
+    const query = c.req.query("q");
+    return c.json({ query: query });
+});
 app.post("/", (c) => c.text("post request."));
 
 app.fire();
