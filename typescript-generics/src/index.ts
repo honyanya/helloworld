bbba419c3d4a75aa7d7a336151a3e1c82082783a
honyanya <matsutani.r.communication@gmail.com>
2022-08-11 23:44:09 +0900

    [add] Generic Types

--- typescript-generics/src/index.ts
+++ typescript-generics/src/index.ts
@@ -1,9 +1,13 @@
-function identity<Type>(arg: Array<Type>): Array<Type> {
-    console.log(arg.length);
+function identity<Type>(arg: Type): Type {
     return arg;
 }
+
+// let myIdentity: <Type>(arg: Type) => Type = identity;
+// let myIdentity: <Input>(arg: Input) => Input = identity;
+let myIdentity: { <Type>(arg: Type): Type } = identity;
 
 // const output = identity<string>("Hello World!");
 const output = identity(["Hello World!"]);
 
 console.info(output);
+  
