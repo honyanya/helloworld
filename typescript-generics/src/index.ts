83712ec6608569732f076003479e25291a6406aa
honyanya <matsutani.r.communication@gmail.com>
2022-08-12 23:52:59 +0900

    [add] debug add

--- typescript-generics/src/index.ts
+++ typescript-generics/src/index.ts
@@ -19,6 +19,8 @@ class GenericNumber<NumType> {
    
 let myGenericNumber = new GenericNumber<number>();
 myGenericNumber.zeroValue = 0;
-myGenericNumber.add = function (x, y) {
+const add = myGenericNumber.add = function (x, y) {
     return x + y;
 };
+
+console.info(add(10,20));
