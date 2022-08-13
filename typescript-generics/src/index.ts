79e2211e1d4ae969dbccf11e6949935e494eacc9
honyanya <matsutani.r.communication@gmail.com>
2022-08-14 00:05:09 +0900

    [add] Generic Constraints Checking the loggingIdentity Function

--- typescript-generics/src/index.ts
+++ typescript-generics/src/index.ts
@@ -33,3 +33,7 @@ function loggingIdentity<Type extends Lengthwise>(arg: Type): Type {
     console.log(arg.length); // Now we know it has a .length property, so no more error
     return arg;
 }
+
+// loggingIdentity(3); // Argument of type 'number' is not assignable to parameter of type 'Lengthwise'.
+
+loggingIdentity({ length: 10, value: 3 });
