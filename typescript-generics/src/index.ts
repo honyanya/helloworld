61f9ddd603ca4a73f0a985798b24daa858f7b123
honyanya <matsutani.r.communication@gmail.com>
2022-08-14 23:53:32 +0900

    [add] Using Type Parameters in Generic Constraints

--- typescript-generics/src/index.ts
+++ typescript-generics/src/index.ts
@@ -37,3 +37,13 @@ function loggingIdentity<Type extends Lengthwise>(arg: Type): Type {
 // loggingIdentity(3); // Argument of type 'number' is not assignable to parameter of type 'Lengthwise'.
 
 loggingIdentity({ length: 10, value: 3 });
+
+function getProperty<Type, Key extends keyof Type>(obj: Type, key: Key) {
+    return obj[key];
+}
+   
+let x = { a: 1, b: 2, c: 3, d: 4 };
+   
+getProperty(x, "a");
+getProperty(x, "b");
+// getProperty(x, "m"); // Argument of type '"m"' is not assignable to parameter of type '"a" | "b" | "c" | "d"'.
