1739fec82099806fb0e4770b7d80f88772194dba
honyanya <matsutani.r.communication@gmail.com>
2022-08-16 23:20:15 +0900

    [change] query paramater

--- graphqljs-hello-world/src/server.js
+++ graphqljs-hello-world/src/server.js
@@ -3,21 +3,21 @@ var { graphql, buildSchema } = require('graphql');
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    hello: String
+    message: String
   }
 `);
 
 // The rootValue provides a resolver function for each API endpoint
 var rootValue = {
-  hello: () => {
-    return 'Hello world!';
+  message: () => {
+    return 'Hello world! GraphQL';
   },
 };
 
-// Run the GraphQL query '{ hello }' and print out the response
+// Run the GraphQL query '{ message }' and print out the response
 graphql({
   schema,
-  source: '{ hello }',
+  source: '{ message }',
   rootValue
 }).then((response) => {
   console.log(response);
