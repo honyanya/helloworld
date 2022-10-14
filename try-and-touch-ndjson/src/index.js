24924e1700951137ccc6c283b8d8396d12c3e404
honyanya <matsutani.r.communication@gmail.com>
2022-10-14 23:13:16 +0900

    [add] ndjson write

--- try-and-touch-ndjson/src/index.js
+++ try-and-touch-ndjson/src/index.js
@@ -1,7 +1,17 @@
 const fs = require("fs");
 const ndjson = require("ndjson");
 
-fs.createReadStream("./src/data.txt")
+const filepath = "./src/data.txt";
+
+const stream = ndjson.stringify();
+stream.on("data", function (line) {
+  // line is a line of stringified JSON with a newline delimiter at the end
+  fs.writeFileSync(filepath, line);
+});
+stream.write({ foo: "bar" });
+stream.end();
+
+fs.createReadStream(filepath)
   .pipe(ndjson.parse())
   .on("data", function (obj) {
     // obj is a javascript object
