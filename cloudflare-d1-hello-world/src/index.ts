2ec057172452e6de78f2fff8eec9f62fc7814666
honyanya <matsutani.r.communication@gmail.com>
2022-11-23 23:13:51 +0900

    [add] endpoint

--- cloudflare-d1-hello-world/src/index.ts
+++ cloudflare-d1-hello-world/src/index.ts
@@ -5,6 +5,7 @@ export interface Env {
 export default {
     async fetch(request: Request, env: Env) {
         const { pathname } = new URL(request.url);
+
         if (pathname === "/api/beverages") {
             const { results } = await env.dev.prepare(
                 "SELECT id, company_name, contact_name FROM customers WHERE company_name = ?"
@@ -13,6 +14,16 @@ export default {
             .all();
             return Response.json(results);
         }
+
+        if (pathname === "/api/futterkiste") {
+            const { results } = await env.dev.prepare(
+                "SELECT id, company_name, contact_name FROM customers WHERE company_name = ?"
+            )
+            .bind("Alfreds Futterkiste")
+            .all();
+            return Response.json(results);
+        }
+
         return new Response(
             "Call /api/beverages to see everyone who works at Bs Beverages"
         );
