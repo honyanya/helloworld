9ca645706858b3b4c8a1f37bc23b37aa05d1a889
honyanya <matsutani.r.communication@gmail.com>
2022-11-30 23:00:18 +0900

    [fix] config

--- nuxt3-hello-world/nuxt.config.ts
+++ nuxt3-hello-world/nuxt.config.ts
@@ -1,13 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  title: 'Hello Nuxt',
-  theme: {
-    dark: true,
-    colors: {
-      primary: '#ff0000'
-    }
-  },
   modules: [
     '@nuxt/ui'
-  ]
+  ],
 })
