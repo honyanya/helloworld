d41f29b47c43bcb0b1743a91e0ef6759a056b09c
honyanya <matsutani.r.communication@gmail.com>
2022-12-10 23:53:10 +0900

    [add] config

--- nuxt3-hello-world/nuxt.config.ts
+++ nuxt3-hello-world/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "nuxt3-hello-world"
+    }
+  },
   modules: [
     '@nuxt/ui'
   ],
