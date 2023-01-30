926840974f804f6ee1efb39ad2521c868406c90c
honyanya <matsutani.r.communication@gmail.com>
2023-01-30 23:56:19 +0900

    [add] clap-hello-world chrono FixedOffset

--- clap-hello-world/src/main.rs
+++ clap-hello-world/src/main.rs
@@ -1,3 +1,4 @@
+use chrono::FixedOffset;
 use chrono::Local;
 use chrono::Utc;
 use clap::Parser;
@@ -29,6 +30,9 @@ fn show_date() {
     let utc_datetime = Utc::now().format("%Y-%m-%d %H:%M:%S %Z").to_string();
     println!("{}", utc_datetime);
 
-    let local_datetime = Local::now().format("%Y-%m-%d %H:%M:%S %Z").to_string();;
+    let local_datetime = Local::now().format("%Y-%m-%d %H:%M:%S %Z").to_string();
     println!("{}", local_datetime);
+
+    let pdt_datetime = Local::now().with_timezone(&FixedOffset::west(7 * 3600)).format("%Y-%m-%d %H:%M:%S %Z").to_string();
+    println!("{}", pdt_datetime);
 }
